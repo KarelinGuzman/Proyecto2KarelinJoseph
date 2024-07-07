bbed67548e32009a30f1e07155d3512d4f54dc68
karelin <>
2024-07-07 03:00:56 -0400

    Avance Interfaces y TXT

--- src/proyecto2karelinjoseph/Interfaces/MenuAnalizarResumen.java
+++ src/proyecto2karelinjoseph/Interfaces/MenuAnalizarResumen.java
@@ -4,17 +4,31 @@
  */
 package proyecto2karelinjoseph.Interfaces;
 
+import proyecto2karelinjoseph.TablaHash;
+
 /**
  *
- * @author andre
+ * @author karelin
  */
 public class MenuAnalizarResumen extends javax.swing.JFrame {
 
+    public static MenuInicial menu;
+    TablaHash tabla;
     /**
      * Creates new form MenuAnalizarResumen
      */
-    public MenuAnalizarResumen() {
+    public MenuAnalizarResumen(MenuInicial menu) {
         initComponents();
+        this.setVisible(true);
+        this.menu = menu;
+        this.tabla = menu.getTablaHash();
+        for (int i = 0; i < tabla.size; i++) {
+            String[] titulos = tabla.archivosResumen[i].mostrar();
+            for (int j = 0; j < titulos.length; j++) {
+                this.resumenes.addItem(titulos[j]);
+
+            }
+        }
     }
 
     /**
@@ -26,21 +40,84 @@ public class MenuAnalizarResumen extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        Guardar = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        resumenes = new javax.swing.JComboBox<>();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+        jLabel2 = new javax.swing.JLabel();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jPanel1.setBackground(new java.awt.Color(0, 51, 51));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setFont(new java.awt.Font("Dialog", 1, 24)); // NOI18N
+        jLabel1.setText("Resumenes:");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 170, -1, -1));
+
+        Guardar.setBackground(new java.awt.Color(255, 255, 255));
+        Guardar.setFont(new java.awt.Font("Dialog", 1, 24)); // NOI18N
+        Guardar.setForeground(new java.awt.Color(0, 51, 51));
+        Guardar.setText("Regresar");
+        Guardar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                GuardarActionPerformed(evt);
+            }
+        });
+        jPanel1.add(Guardar, new org.netbeans.lib.awtextra.AbsoluteConstraints(740, 20, -1, -1));
+
+        jButton4.setBackground(new java.awt.Color(255, 255, 255));
+        jButton4.setFont(new java.awt.Font("Dialog", 1, 24)); // NOI18N
+        jButton4.setForeground(new java.awt.Color(0, 51, 51));
+        jButton4.setText("Analizar");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 280, -1, -1));
+
+        resumenes.setBackground(new java.awt.Color(255, 255, 255));
+        resumenes.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
+        resumenes.setForeground(new java.awt.Color(0, 51, 51));
+        jPanel1.add(resumenes, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 210, 400, 40));
+
+        jTextArea1.setEditable(false);
+        jTextArea1.setBackground(new java.awt.Color(255, 255, 255));
+        jTextArea1.setColumns(20);
+        jTextArea1.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
+        jTextArea1.setForeground(new java.awt.Color(0, 51, 51));
+        jTextArea1.setRows(5);
+        jScrollPane1.setViewportView(jTextArea1);
+
+        jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(550, 140, -1, 350));
+
+        jLabel2.setFont(new java.awt.Font("Dialog", 1, 48)); // NOI18N
+        jLabel2.setText("ANALIZAR RESUMEN");
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 40, -1, -1));
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 910, 510));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        // TODO add your handling code here:
+        try{
+            String titulo = this.resumenes.getSelectedItem().toString();
+            this.jTextArea1.setText(this.tabla.buscarPorTitulo(titulo).analizarArchivo());
+        }catch(Exception e){}
+    }//GEN-LAST:event_jButton4ActionPerformed
+
+    private void GuardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_GuardarActionPerformed
+        // TODO add your handling code here:
+        this.menu.setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_GuardarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -72,11 +149,19 @@ public class MenuAnalizarResumen extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new MenuAnalizarResumen().setVisible(true);
+                new MenuAnalizarResumen(menu).setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton Guardar;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JComboBox<String> resumenes;
     // End of variables declaration//GEN-END:variables
 }
